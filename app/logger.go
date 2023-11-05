2d6da78bfaeb323aa4b58a2c63b91038250181e6
Momchil Atanasov <momchil.dev@gmail.com>
2023-11-05 16:32:11 +0200

    Add metrics

--- app/logger.go
+++ app/logger.go
@@ -3,6 +3,5 @@ package app
 import "github.com/mokiat/lacking/debug/log"
 
 var (
-	appLogger = log.Namespace("lacking-native/app")
-	glLogger  = log.Namespace("opengl")
+	glLogger = log.Namespace("opengl")
 )
