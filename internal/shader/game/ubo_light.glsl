00d4287a2a83b8cb154b3106c5b5b9d9c1c7f07e
Momchil Atanasov <momchil.dev@gmail.com>
2024-09-14 16:34:56 +0300

    Reduce max cascades

--- internal/shader/game/ubo_light.glsl
+++ internal/shader/game/ubo_light.glsl
@@ -1,8 +1,8 @@
 layout (std140) uniform Light
 {
-	mat4 lackingLightShadowMatrices[8];
+	mat4 lackingLightShadowMatrices[4];
 	mat4 lackingLightModelMatrix;
-	vec2 lackingLightShadowCascades[8];
+	vec2 lackingLightShadowCascades[4];
 	vec4 lightIntensityIn;
 	vec4 lightSpanIn;
 };
