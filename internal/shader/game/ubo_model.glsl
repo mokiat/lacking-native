bf099a3b3129236dc8e04f84abd92e2adaf9b675
Momchil Atanasov <momchil.dev@gmail.com>
2025-05-22 15:38:00 +0300

    Switch to translator from lacking

--- internal/shader/game/ubo_model.glsl
+++
@@ -1,4 +0,0 @@
-layout (std140) uniform Model
-{
-	mat4 modelMatrixIn[256];
-};
--- render/program.go
+++ render/program.go
@@ -1,25 +1,8 @@
 package render
 
 import (
-	"strings"
-
-	"github.com/mokiat/lacking/render"
+	"github.com/mokiat/lacking/game/graphics/glsl"
 )
 
-// ProgramCode is an implementation of render.ProgramCode that can be used
-// with this render API implementation.
-type ProgramCode struct {
-	render.ProgramCodeMarker
-
-	// VertexCode specifies the vertex shader code.
-	VertexCode string
-
-	// FragmentCode specifies the fragment shader code.
-	FragmentCode string
-}
-
-func (p ProgramCode) String() string {
-	vertex := strings.ReplaceAll(p.VertexCode, "\n\n", "\n")
-	fragment := strings.ReplaceAll(p.FragmentCode, "\n\n", "\n")
-	return vertex + "\n\n---\n\n" + fragment
-}
+// TODO: Replace completely with glsl.ProgramCode
+type ProgramCode = glsl.ProgramCode
