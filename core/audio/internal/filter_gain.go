cb5f9ac5a47a733d78787b8125fd7013556d66c2
Momchil Atanasov <momchil.dev@gmail.com>
2026-06-06 00:26:07 +0300

    audio: add godoc to gain filter

--- core/audio/internal/filter_gain.go
+++ core/audio/internal/filter_gain.go
@@ -6,6 +6,8 @@ import (
 	"github.com/mokiat/lacking/core/audio"
 )
 
+// GainFilter is a stereo Processor that scales each frame by a gain factor.
+// The gain is safe to set from any goroutine.
 type GainFilter struct {
 	// The following fields are protected by the mutex and can be accessed from
 	// any thread.
@@ -15,12 +17,14 @@ type GainFilter struct {
 
 var _ Processor = (*GainFilter)(nil)
 
+// NewGainFilter creates a GainFilter with unity gain.
 func NewGainFilter() *GainFilter {
 	return &GainFilter{
 		gain: 1.0,
 	}
 }
 
+// Gain returns the current gain factor.
 func (f *GainFilter) Gain() float32 {
 	f.mu.Lock()
 	defer f.mu.Unlock()
@@ -28,6 +32,7 @@ func (f *GainFilter) Gain() float32 {
 	return f.gain
 }
 
+// SetGain sets the gain factor, clamped to a minimum of zero.
 func (f *GainFilter) SetGain(gain float32) {
 	f.mu.Lock()
 	defer f.mu.Unlock()
@@ -35,6 +40,7 @@ func (f *GainFilter) SetGain(gain float32) {
 	f.gain = max(0.0, gain)
 }
 
+// Process applies the gain to inputFrames and returns the result.
 func (f *GainFilter) Process(ctx ProcessContext, inputFrames []audio.Frame) []audio.Frame {
 	f.mu.Lock()
 	gain := f.gain // store value locally to avoid long locks
