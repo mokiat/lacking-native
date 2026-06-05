255b5b9940cf0a1e4427660950813a7a078f3a51
Momchil Atanasov <momchil.dev@gmail.com>
2026-06-05 23:45:20 +0300

    audio: add godoc to media

--- core/audio/internal/media.go
+++ core/audio/internal/media.go
@@ -2,6 +2,7 @@ package internal
 
 import "github.com/mokiat/lacking/core/audio"
 
+// Media holds decoded audio frames and their sample rate.
 type Media struct {
 	sampleRate int
 	frames     []audio.Frame
@@ -9,6 +10,7 @@ type Media struct {
 
 var _ audio.Media = (*Media)(nil)
 
+// NewMedia creates a Media from decoded audio data.
 func NewMedia(data audio.MediaData) *Media {
 	return &Media{
 		sampleRate: data.SampleRate,
@@ -16,11 +18,13 @@ func NewMedia(data audio.MediaData) *Media {
 	}
 }
 
+// Length returns the duration of the media in seconds.
 func (m *Media) Length() float32 {
 	frameCount := len(m.frames)
 	return audio.Seconds(frameCount, m.sampleRate)
 }
 
+// Release frees the frame data. Existing playbacks are not affected.
 func (m *Media) Release() {
 	m.frames = nil
 }
